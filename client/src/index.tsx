dba75fa66ac8c9201468504eca3023a4300848f2
imk-student <160251346+imk-student@users.noreply.github.com>
2024-03-29 15:56:04 +0300

    Revert "merge main"

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Popup from './components/editPopup/editPopup';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -11,7 +10,6 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <App />
-    <Popup />
   </React.StrictMode>
 );
 
