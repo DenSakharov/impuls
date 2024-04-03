b9bd46f3a6aab947fda9b50321cfcb3231c715b1
Ilya <ilya.krasnenkov@mail.ru>
2024-04-04 02:39:32 +0300

    Добавил изменение формы по нажатии на кнопку "Добавить новое поле", теперь появляются дополн- ительные поля, которые можно редактировтаь. Добавил изменение стейта значения Статус по нажатию на соответствующие кнопки Передал объект в качестве параметра props на форму

--- client/src/components/editPopup/editPopup.tsx
+++ client/src/components/editPopup/editPopup.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
 import './styles/edit_popup_window.css'
 import EditPopupProps from '../interfaces/editPopupProps';
+import { link } from 'fs';
 
 function EditPopup(props: EditPopupProps) {
+    const [status, setStatus] = React.useState(props.status);
+    const changeStatus = (value: string) => {
+        setStatus((status) => status = value);
+    }
 
+    const [tags, setTags] = React.useState(props.tags);
+    const addTags = (value: {key:string, value:string | number}[]) => {
+        setTags((tags) => tags = value);
+        console.log(tags)
+    }
+
+    const updateTag = (value: {key:string, value:string | number}, event: any) => {
+        setTags(tags.map(tag => {
+            if (tag.key === value.key) {
+                return {key: value.key, value: event.target.value}
+            } else {
+                return tag;
+            }
+        }));
+    }
+
+    
   return (
     <div id="edit_popup_window">
         <div id="popup_top_panel"> 
@@ -17,33 +39,42 @@ function EditPopup(props: EditPopupProps) {
                 <div id="attr_header">
                     <p>№ {props.id}</p>
                     <p>{props.name}</p>
-                    <span> Дата создания {props.date_created.toString()} </span>
-                    <span> Дата изменения {props.date_changed.toString()} </span>
+                    <span> Дата создания {props.date_created.toLocaleDateString()} </span>
+                    <span> Дата изменения {props.date_changed.toLocaleDateString()} </span>
                     <hr/>
                 </div>
                 <div id="field_attributes">
                     <p><label>Описание</label> <textarea id="large_input">{props.desc}</textarea></p>
                     <p><label>Автор</label> <input id="wide_input" value={props.author}/> </p>
-                    <p id="status_p"><label>Статус</label> <input value={props.status}/>
-                        <button id="accept_offer_button">На утверждение</button>
-                        <button id="rework_button">На доработку</button>
-                        <button id="accept_button">Утвердить</button>
-                        <button id="inwork_button">В разработке</button>                    
+                    <p id="status_p"><label>Статус</label> <input value={status}/>
+                        <button onClick={() =>changeStatus("На утверждение")} id="accept_offer_button">На утверждение</button>
+                        <button onClick={() =>changeStatus("На доработку")} id="rework_button">На доработку</button>
+                        <button onClick={() =>changeStatus("Утвердить")} id="accept_button">Утвердить</button>
+                        <button onClick={() =>changeStatus("В разработке")} id="inwork_button">В разработке</button>                    
                     </p>
                     <p><label>Тип</label> <input value={props.type}/> <button id="edit_button">Изменить</button></p> 
                     <p><label>Приоритет</label> <input id="wide_input" value={props.priority}/></p>
 
                     <p><label>Вложения</label> <input id="wide_input"/> <button id="edit_button">Изменить</button></p>
-                    <p><label>Путь</label> <input id="wide_input"/> <button id="edit_button">Изменить</button></p>
+                    <p><label>Путь</label> <input id="wide_input" value={props.path}/> <button id="edit_button">Изменить</button></p>
                 </div>
                 <div id="button_attributes">
                     <div id="add_links">
                         <img src='img/plus_black.png'/> Добавить ссылку
-                        <p id="link_attr"><a>https://learn.javascript.ru/</a></p>
+                        {props.link.map((link) => (
+                            <p id="link_attr"><a>{link}</a></p>
+                        ))}
                     </div>
                     <div id="add_fields">
-                        <img src='img/plus_black.png'/> Добавить новое поле
-                        <p id="field_attr"><label></label>Тег <input/></p>
+                        <div onClick={() => addTags([...tags, {key: 'Тэг'+(tags.length+1), value: ""}])}><img src='img/plus_black.png'/> Добавить новое поле</div>
+                        {tags.map((tag) => (
+                            <p id="field_attr"><label></label> 
+                            {tag.key}
+                            <input 
+                                value={tag.value}
+                                onChange={(event) => updateTag(tag, event)}
+                                /></p>
+                        ))}
                     </div>
                     <div id="doc_dependencies">
                         <img src='img/plus_black.png'/> Зависимости
