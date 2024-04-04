78f6dfe52136813f8e7746672da87717b1dc2f29
Ilya <ilya.krasnenkov@mail.ru>
2024-04-04 12:13:08 +0300

    Reapply "Добавил изменение формы по нажатии на кнопку"
    This reverts commit cad3c655bc2dd0bcbf12c3c031ba501bebbd6bd3.

--- client/src/components/editPopup/editPopup.tsx
+++ client/src/components/editPopup/editPopup.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './styles/edit_popup_window.css'
 import EditPopupProps from '../interfaces/editPopupProps';
+import { link } from 'fs';
 
 function EditPopup(props: EditPopupProps) {
     const [status, setStatus] = React.useState(props.status);
@@ -38,28 +39,30 @@ function EditPopup(props: EditPopupProps) {
                 <div id="attr_header">
                     <p>№ {props.id}</p>
                     <p>{props.name}</p>
-                    <span> Дата создания {props.date_created.toString()} </span>
-                    <span> Дата изменения {props.date_changed.toString()} </span>
+                    <span> Дата создания {props.date_created.toLocaleDateString()} </span>
+                    <span> Дата изменения {props.date_changed.toLocaleDateString()} </span>
                     <hr/>
                 </div>
                 <div id="field_attributes">
                     <p><label>Описание</label> <textarea id="large_input">{props.desc}</textarea></p>
                     <p><label>Автор</label> <input id="wide_input" value={props.author}/> </p>
-                    <p id="status_p"><label>Статус</label> <input value={props.status}/>
-                        <button id="accept_offer_button">На утверждение</button>
-                        <button id="rework_button">На доработку</button>
-                        <button id="accept_button">Утвердить</button>
-                        <button id="inwork_button">В разработке</button>                    
+                    <p id="status_p"><label>Статус</label> <input value={status}/>
+                        <button onClick={() =>changeStatus("На утверждение")} id="accept_offer_button">На утверждение</button>
+                        <button onClick={() =>changeStatus("На доработку")} id="rework_button">На доработку</button>
+                        <button onClick={() =>changeStatus("Утвердить")} id="accept_button">Утвердить</button>
+                        <button onClick={() =>changeStatus("В разработке")} id="inwork_button">В разработке</button>                    
                     </p>
                     <p><label>Тип</label> <input value={props.type}/> <button id="edit_button">Изменить</button></p> 
                     <p><label>Приоритет</label> <input id="wide_input" value={props.priority}/></p>
                     <p><label>Вложения</label> <input id="wide_input"/> <button id="edit_button">Изменить</button></p>
-                    <p><label>Путь</label> <input id="wide_input"/> <button id="edit_button">Изменить</button></p>
+                    <p><label>Путь</label> <input id="wide_input" value={props.path}/> <button id="edit_button">Изменить</button></p>
                 </div>
                 <div id="button_attributes">
                     <div id="add_links">
                         <img src='img/plus_black.png'/> Добавить ссылку
-                        <p id="link_attr"><a>https://learn.javascript.ru/</a></p>
+                        {props.link.map((link) => (
+                            <p id="link_attr"><a>{link}</a></p>
+                        ))}
                     </div>
                     <div id="add_fields">
                         <div onClick={() => addTags([...tags, {key: 'Тэг'+(tags.length+1), value: ""}])}><img src='img/plus_black.png'/> Добавить новое поле</div>
