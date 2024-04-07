f29dcf585f26024de6944fa8ed63fc2d9f90ff29
Ilya <ilya.krasnenkov@mail.ru>
2024-04-07 21:23:31 +0300

    убрал из app.tsx объект для тестов, перенес его в значнеие по умолчанию для edit editPopup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,24 +7,6 @@ import About from './components/about/about';
 import LoginPage from './components/loginPage/loginPage';
 
 function App() {
-
-  const popupObj = {
-    id: 10000,
-    name: "Документ под названием “Объект 1.2”",
-    desc: "Описание документа 10000 для создания папапа при создании документа",
-    author: "Красненков Илья",
-    status: "В разработке",
-    date_created: new Date("2024-01-01"),
-    date_changed: new Date("2024-03-01"),
-    type: "Основной документ",
-    priority: "всевышний",
-    path: "S://impuls/client/src/components/testPage/testPage.tsx",
-    links: ["https://learn.javascript.ru/","https://google.com"],
-    tags: [{ key: "Тэг1", value: "Значение тэга" }, { key: "Тэг2", value: 2 }],
-  }
-
-
-
   return (
       <BrowserRouter>
         <Routes>
@@ -32,7 +14,7 @@ function App() {
           <Route path='/test' Component={TestPage}></Route>
           <Route path='/main' Component={MainPage}></Route>
           <Route path='/about' Component={About}></Route>
-          <Route path='/popup' element={EditPopup(popupObj)}></Route>
+          <Route path='/popup' element={EditPopup()}></Route>
         </Routes>
       </BrowserRouter>
   );
