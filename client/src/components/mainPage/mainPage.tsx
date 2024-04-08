d3cabface87beadb62d6ee5b8327309f5bc73315
Sergey Kozhevnikov <SYKozhevnikov@rasu.ru>
2024-04-09 02:24:24 +0300

    Создание главной формы mainPage

--- client/src/components/mainPage/mainPage.tsx
+++ client/src/components/mainPage/mainPage.tsx
@@ -1,17 +1,43 @@
 import React from 'react';
-import './mainPage.css';
-
+import './stylesMainPage.css';
 
 function mainPage() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src='./img/logo.png' className="App-logo1" alt="logo" />
-        <p>
-          Добро пожаловать в приложение "Импульс"
-        </p>
-    <h1>Проект IMS</h1>
-    <a
+<div className="mainApp mainApp-push-bottom">
+<header className="header">
+
+    <nav id="cssmenu">
+		<ul>
+			<li>
+          <img src='./img/logo.png' className="mainApp-logo" alt="logo" width="50" height="50" />
+			</li>
+      <li>
+        <div>
+         <p id="h3">IMS Impuls </p>
+         <p>Project №1</p>
+        </div>
+			</li>
+			<li>
+      <a href='index.html' target=""><span>Home</span></a>
+			</li>
+			<li>
+				<a href='index.html' target=""><span>Меню1</span></a>
+			</li>
+			<li className='active has-sub'><a href='#'><span>Меню2</span></a>
+				<ul>
+					<li className='has-sub'><a href='index.html' target=""><span>под меню21</span></a></li>
+					<li className='has-sub'><a href='index.html' target=""><span>под меню22</span></a></li>
+					<li className='has-sub'><a href='index.html' target=""><span>под меню23</span></a></li>
+					<li className='has-sub'><a href='index.html' target=""><span>под меню24</span></a></li>
+				</ul>
+			</li>
+			<li className='last'><a href='index.html' target=""><span>Меню3</span></a>
+			</li>
+      <li className='last'><a href='index.html' target=""><span>Меню4</span></a>
+			</li>
+
+      <li className='last'>
+      <a
           className="App-link"
           href="http://localhost:3000"
           target=""
@@ -19,17 +45,72 @@ function mainPage() {
         >
           Сменить пользователя
         </a>
-        <a
+       </li>
+       <li className='last'><a
           className="App-link"
           href="http://localhost:3000/about"
           target=""
           rel="noopener noreferrer"
         >
           О программе
-        </a>
-      </header>
+          </a>
+			</li>
+		</ul>
+  </nav>
+
+</header>
+
+<div className ="clear"/>
+
+<main className='section'>
+  <div className='container-app'>
+
+    <div className="aside">
+   	Left side
+  	</div>
+
+	  <div className="content">
+      <div id="containerHeader">
+	     <div id="mainHeader">
+			  <div className="content-text-block">
+   			  <p> Дашборд проекта</p>
+			  </div>
+    	 </div>
+
+      <div id="container-news">
+      <p>Новости</p>
+   	  </div>
+
+      <div id="container-kpi">
+      <p>Ключевые показатели</p>
+   	  </div>
+
+      <div id="container-proj">
+      <p>Информация проекта</p>
+   	  </div>
+
     </div>
-  );
+
+
+	  </div>
+
+  </div>
+</main>
+
+<div className ="clear"/>
+
+<footer id="content">
+    <div id='foot'>
+      <p> <a href="">О программе </a> | <a href="">Контакты </a> </p>
+      <p id="copyright"><a href="">© Impuls Teem 2024</a></p>
+    </div>
+
+</footer>
+
+</div>
+
+
+);
 }
 
 export default mainPage;
