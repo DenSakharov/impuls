32264dd1fa10da1a20badce6376d284e8e8ead55
Ilya <ilya.krasnenkov@mail.ru>
2024-04-08 12:41:42 +0300

    Внез изменения по чату

--- client/src/components/editPopup/editPopup.tsx
+++ client/src/components/editPopup/editPopup.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import './styles/edit_popup_window.css'
 import EditPopupProps from '../interfaces/editPopupProps';
 import ChatMessage from './chatMessage';
+import Chat from './chat';
+import message from '../interfaces/messageProps';
 
     
 
@@ -21,34 +23,12 @@ function EditPopup(props: EditPopupProps = {
   }) 
 
   {
-    const messages = [{  
-        id: 1,
-        author: "Илья Красненков",
-        date_sent: new Date("2024-01-01"),
-        text: "Текст сообщения 1",
-        avatar: "https://avatars.mds.yandex.net/get-yapic/23186/enc-fcff59d213e265d10a2cccb679221e95c9b92a7e71c02c7c269cd6e384249449/islands-retina-middle"
-    },
-    {  
-        id: 2,    
-        author: "Кожевников Сергей",
-        date_sent: new Date("2024-01-02"),
-        text: "Текст сообщения 2",
-        avatar: "https://cdn.discordapp.com/avatars/1206913485267664947/ea519d3cd94003890fd84eec29d8e720.webp?size=80"
-    },
-    {  
-        id: 3,    
-        author: "Илья Красненков",
-        date_sent: new Date("2024-01-03"),
-        text: "Текст сообщения 3",
-        avatar: "https://avatars.mds.yandex.net/get-yapic/23186/enc-fcff59d213e265d10a2cccb679221e95c9b92a7e71c02c7c269cd6e384249449/islands-retina-middle"
-    }
-
-]   
+    
     const statusButtons = [{value: 'На утверждение', style: 'accept_offer_button'},
                            {value: 'На доработку', style: 'rework_button'},
                            {value: 'Утвердить', style: 'accept_button'},
                            {value: 'В разработке', style: 'inwork_button'}]
-    const [messageArr, setMessages] = React.useState(messages);
+    
     const [tags, setTags] = React.useState(props.tags);
     const [links, setLinks] = React.useState(props.links)
     const [file, setFile] = React.useState(null)
@@ -70,19 +50,7 @@ function EditPopup(props: EditPopupProps = {
         }
     }
 
-    const sendMessage = (event: any) => {
-        if (event.key === "Enter" && event.target.value !== "") {
-            event.preventDefault();
-            setMessages([...messageArr, {
-                id: messageArr.length + 1,
-                author: "Илья Красненков",
-                date_sent: new Date(),
-                text: event.target.value,
-                avatar: "https://avatars.mds.yandex.net/get-yapic/23186/enc-fcff59d213e265d10a2cccb679221e95c9b92a7e71c02c7c269cd6e384249449/islands-retina-middle"
-            }])
-            event.target.value = "";
-        }
-    }
+    
     const handleFileChange = (event: any) => {
         const files = event.target.files;
         if (files.length > 0) {
@@ -159,23 +127,8 @@ function EditPopup(props: EditPopupProps = {
                 </div>
             </div>
             <div id="chat_block">
-                <div id="chat_header">
-                    <p id="header_text">Обсуждение</p>
-                    <hr id="chat_line"/>
-                </div>
-               <div id="chat_log">
-                    <div id="chat_messages">
-                        {messageArr.map((message) => (
-                            <ChatMessage {...message} key={messageArr.indexOf(message)}/>
-                        ))}
-                    </div>
-                </div>
-                <div id="message_field">
-                    <hr id="chat_line"/>
-                    <textarea id="chat_input" onKeyDown={(event) => sendMessage(event)}></textarea>
-                </div>
+                <Chat/>
             </div>
-
         </div>
     </div>
   );
