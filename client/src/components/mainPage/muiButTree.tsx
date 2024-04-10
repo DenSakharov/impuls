49e0a298094e13f66cadb39449e4e1c868351f7d
Sergey Kozhevnikov <SYKozhevnikov@rasu.ru>
2024-04-11 01:01:48 +0300

    добавил butTree

---
+++ client/src/components/mainPage/muiButTree.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import data from '../editPopup/data'
+import { IconButton, Stack } from '@mui/material';
+import { Settings, Info, Favorite, ContentCopy, Add } from '@mui/icons-material';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+
+function MuiButTree() {
+    // const classes = useStyles()
+
+    return (
+        <Stack spacing={1} direction="row">
+        <IconButton aria-label="settings" color='default' size="small"><Add fontSize="small"/></IconButton>
+        <IconButton aria-label="settings" color='default' size="small"><ContentCopy fontSize="small" /></IconButton>
+        <IconButton edge='end' aria-label="delete" color='default' size="small"> <DeleteIcon fontSize="small"/></IconButton>
+        <IconButton aria-label="info" color='default' size="small"><Info fontSize="small"/></IconButton>
+        <IconButton aria-label="settings" color='default' size="small"><Settings fontSize="small"/></IconButton>
+        </Stack>
+    );
+  }
+   
+  export default MuiButTree;
