e48e4c46a5be1e03d5650604c091a29e47521dad
Ilya <ilya.krasnenkov@mail.ru>
2024-04-10 23:55:38 +0300

    Добавил окно при нажатии на название проекта

--- client/src/components/editPopup/data.js
+++ client/src/components/editPopup/data.js
@@ -101,6 +101,60 @@
             {
                 name: "Задача 3",
             }]
+    },
+    {
+        name : "Проект 2",
+        objects: [{
+            name: "Объект 11",
+            },
+            {
+                name: "Объект 22",
+            },
+            {
+                name: "Объект 34",
+            }],
+                documents: [{
+                    name: "Документ 1",
+                },
+                {
+                    name: "Документ 22",
+                },
+                {
+                    name: "Документ 3",
+                    data: [{
+                        name: "Объект 10000",
+                        object :{
+                        id: 10000,
+                        name: "Документ под названием “Объект 1.2”",
+                        desc: "Описание документа 10000 для создания чего либо при создании документа",
+                        author: "Красненков Илья",
+                        status: "В разработке",
+                        date_created: new Date("2024-01-01"),
+                        date_changed: new Date("2024-03-01"),
+                        type: "Основной документ",
+                        priority: "всевышний",
+                        path: "S://impuls/client/src/components/testPage/testPage.tsx",
+                        links: ["https://learn.javascript.ru/","https://google.com"],
+                        tags: [{ key: "Тэг1", value: "Значение тэга" }, { key: "Тэг2", value: 2 }],
+                        }
+                    },
+                    {
+                        name: "Документ 3.2",
+                    },
+                    {
+                        name: "Документ 3.3",
+                    }
+                ]
+            }],
+            tasks: [{
+                name: "Задача 11",
+            },
+            {
+                name: "Задача 22",
+            },
+            {
+                name: "Задача 33",
+            }]
     }]
 }
 
