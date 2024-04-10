5ee5c1f0b143cf6a25664365a8c166b8775780c8
Ilya <ilya.krasnenkov@mail.ru>
2024-04-10 23:55:38 +0300

    Добавил окно при нажатии на название проекта

--- client/src/components/mainPage/muiDialog.tsx
+++ client/src/components/mainPage/muiDialog.tsx
@@ -1,48 +1,30 @@
-import * as React from 'react';
+import React from 'react';
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
+import data from '../editPopup/data';
+import { Container } from '@mui/material';
 
-export default function ProjectDialog() {
-  const [open, setOpen] = React.useState(false);
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
+export default function ProjectDialog({formOpen, handleCloseForm, changeProps}: any) {
   return (
     <>
-      <Button variant="outlined" onClick={handleClickOpen}>
-        Open alert dialog
-      </Button>
       <Dialog
-        open={open}
-        onClose={handleClose}
+        open={formOpen}
+        onClose={handleCloseForm}
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
       >
         <DialogTitle id="alert-dialog-title">
-          {"Use Google's location service?"}
+          {"Выберите проект"}
         </DialogTitle>
         <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            Let Google help apps determine location. This means sending anonymous
-            location data to Google, even when no apps are running.
-          </DialogContentText>
+          <Container sx={{display: 'flex', flexDirection: 'column', alignItems: 'flex-start', width: 300}}>
+          {data.tree.map((item: any) => (
+            <Button onClick={() => changeProps(item)}>{item.name}</Button>
+          ))}
+          </Container>
         </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Disagree</Button>
-          <Button onClick={handleClose} autoFocus>
-            Agree
-          </Button>
-        </DialogActions>
       </Dialog>
     </>
   );
