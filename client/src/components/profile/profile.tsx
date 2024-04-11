dfaea5b270cc6641d7dccd319312c1a0f003bf18
RusDa256 <andrey-zharkov@ro.ru>
2024-04-11 16:42:57 +0300

    add href on button Профиль in main page, add adaptive profile

--- client/src/components/profile/profile.tsx
+++ client/src/components/profile/profile.tsx
@@ -4,75 +4,74 @@ import bBack from './img/bBack.svg'
 function Profile() {
   return(
     <div id="page-profile">
-      <div id="wrapper">
-		    <header id="header-profile">
-          <div id="header-left_site">
-            <a id="bBack-profile" href="/">
-              <img src={bBack}/>
-            </a>
-            <h1>Мой профиль</h1>
-          </div>
-          <div id="mainInfo">
-            <p>Имя Фамилия</p>
-            <p>пользователь/админ</p>
-          </div>
-        </header>
-
-        <div id="infoProfile">
-          <div id="editPhoto">
-            <img src=""/>
+      <header id="header-profile">
+        <div id="header-left_site">
+          <a id="bBack" href="">
+            <img src={bBack}/>
+          </a>
+          <h1>Мой профиль</h1>
+        </div>
+        <div id="header-mainInfo">
+          <p>Имя Фамилия</p>
+          <p>пользователь/админ</p>
+        </div>
+      </header>
+      <div id="infoProfile">
+        <div id="editPhoto">
+          <img src=""/>
+          <div id="infoProfile-editPhoto-buttons">
             <button id="bLoadImg" type="button">Загрузить фото</button>
             <button id="bDelImg" type="button">Удалить</button>
           </div>
+        </div>
 
-          <div id="infoUser">
-            <div className="infoUserValues" id="el1">
-              <label>Логин</label>
-              <input type="text"/>
-            </div>
-            <div className="infoUserValues" id="el2">
-              <label>Email</label>
-              <input type="text"/>
-            </div>
-            <div className="infoUserValues" id="el3">
-              <label>Фамилия</label>
-              <input type="text"/>
-            </div>
-            <div className="infoUserValues" id="el4">
-              <label>Телефон</label>
-              <input type="text"/>
-            </div>
-            <div className="infoUserValues" id="el5">
-              <label>Имя</label>
-              <input type="text"/>
-            </div>
-            <div className="infoUserValues" id="el6">
-              <label>Должность</label>
-              <input type="text"/>
-            </div>
-            <div className="infoUserValues" id="el7">
-              <label>Отчество</label>
-              <input type="text"/>
-            </div>
-            <div className="infoUserValues" id="el8">
-              <label>Город</label>
-              <input type="text"/>
-            </div>
-            <div className="infoUserValues" id="el9">
-              <label>Роль</label>
-              <input type="text"/>
-            </div>
-            <div className="infoUserValues" id="el10">
-              <label>Пол</label>
-              <input type="text"/>
-            </div>
-            <div id="replacePas_Save">
-              <a href="/replace_password" id="bReplacePas">Изменить пароль</a>
-              <button id="bSave-profile" type="button">Сохранить</button>
-            </div>
+        <div id="infoUser">
+          <div className="infoUserValues" id="el1">
+            <label>Логин</label>
+            <input type="text"/>
+          </div>
+          <div className="infoUserValues" id="el2">
+            <label>Email</label>
+            <input type="text"/>
+          </div>
+          <div className="infoUserValues" id="el3">
+            <label>Фамилия</label>
+            <input type="text"/>
+          </div>
+          <div className="infoUserValues" id="el4">
+            <label>Телефон</label>
+            <input type="text"/>
+          </div>
+          <div className="infoUserValues" id="el5">
+            <label>Имя</label>
+            <input type="text"/>
+          </div>
+          <div className="infoUserValues" id="el6">
+            <label>Должность</label>
+            <input type="text"/>
+          </div>
+          <div className="infoUserValues" id="el7">
+            <label>Отчество</label>
+            <input type="text"/>
+          </div>
+          <div className="infoUserValues" id="el8">
+            <label>Город</label>
+            <input type="text"/>
+          </div>
+          <div className="infoUserValues" id="el9">
+            <label>Роль</label>
+            <input type="text"/>
+          </div>
+          <div className="infoUserValues" id="el10">
+            <label>Пол</label>
+            <input type="text"/>
+          </div>
+          <div id="replacePas_Save">
+            <a href="/replace_password" id="bReplacePas">Изменить пароль</a>
+            <button id="bSave" type="button">Сохранить</button>
           </div>
         </div>
-	    </div>
+      </div>
     </div>
   )
 }
