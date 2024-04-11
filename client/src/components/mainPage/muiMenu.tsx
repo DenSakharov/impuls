27d47e2b72d5d719fa397c3e93025978222eddeb
Ilya <ilya.krasnenkov@mail.ru>
2024-04-11 16:38:38 +0300

    Исправил все на что ругалась CI должно заработать

--- client/src/components/mainPage/muiMenu.tsx
+++ client/src/components/mainPage/muiMenu.tsx
@@ -1,4 +1,4 @@
-import React, { Profiler } from 'react';
+import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
