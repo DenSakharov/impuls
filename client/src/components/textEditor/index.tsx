0c206a99e7493a8bce527f5f89ae4e4caf870b32
A M <Alcyonespace@gmail.com>
2024-04-12 23:35:10 +0300

    tsx added

---
+++ client/src/components/textEditor/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles.module.scss';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+
+reportWebVitals();
