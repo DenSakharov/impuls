5d49216e734090c4646ac1b4525302331f4ff6e9
A M <Alcyonespace@gmail.com>
2024-04-13 10:55:54 +0300

    Внедрение компонента TextEditor

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import TestPage from './components/testPage/testPage';
 import Profile from './components/profile/profile';
 import ReplacePassword from './components/profile/replacePassword';
@@ -7,7 +7,8 @@ import MainPage from './components/mainPage/mainPage';
 import About from './components/about/about';
 import LoginPage from './components/loginPage/loginPage';
 import MuiPopup from './components/muiPopup/muiPopup';
-
+import TextEditor from './components/textEditor/TextEditor';
+import { v4 as uuidV4 } from 'uuid'
 
 function App() {
   return (
@@ -20,6 +21,8 @@ function App() {
           <Route path='/popup' element={MuiPopup()}></Route>
           <Route path='/profile' Component={Profile}></Route>
           <Route path='/replace_password' Component={ReplacePassword}></Route>
+          <Route path="/documents" element={<Navigate to={`/documents/${uuidV4()}`} />} />
+          <Route path="/documents/:id" element ={<TextEditor />} />
         </Routes>
       </BrowserRouter>
   );
