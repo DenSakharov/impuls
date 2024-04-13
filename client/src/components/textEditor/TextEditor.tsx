7b47765897178ef40e54bc0e8b96f4c3b827ff2a
Ilya <ilya.krasnenkov@mail.ru>
2024-04-13 12:35:32 +0300

    Наладил работоспособность компонента TextEditor

--- client/src/components/textEditor/TextEditor.tsx
+++ client/src/components/textEditor/TextEditor.tsx
@@ -1,11 +1,11 @@
-import React, { SetStateAction, useCallback, useEffect, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import "quill/dist/quill.snow.css"
 import Quill from "quill";
 import {io} from 'socket.io-client'
 import {Socket} from 'socket.io-client'
 import {DefaultEventsMap} from "@socket.io/component-emitter"
 import { useParams } from 'react-router-dom';
-
+import './styles/text_editor.css'
    
 const SAVE_INTERVAL_MS = 2000
 
