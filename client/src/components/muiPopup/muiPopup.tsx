7dac2861b0d5cdd5a23557c20edb6b6e270de1d8
Ilya <ilya.krasnenkov@mail.ru>
2024-04-15 01:54:42 +0300

    Добавил тему и сделал рефакторинг

--- client/src/components/muiPopup/muiPopup.tsx
+++ client/src/components/muiPopup/muiPopup.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import EditPopupProps from '../interfaces/editPopupProps';
-//import muiHistChanges from '../editPopup/histChanges';
 import PopupBar from './popupBar';
+import MuiDialog from './muiDialog';
 import data from '../editPopup/data';
 import { Box, Button, ButtonGroup, Container, Grid, Stack, TextField, Typography, Divider } from '@mui/material';
 import { Add } from '@mui/icons-material';
-import MuiDialog from './muiDialog';
+import impulsTheme from '../../muiTheme';
+import { ThemeProvider } from '@emotion/react';
 
 
 
@@ -74,13 +75,14 @@ export default function MuiPopup(props: EditPopupProps = data.object) {
     
   return (
     <>
+    <ThemeProvider theme={impulsTheme}>
         <Container disableGutters sx={{
-            height:'900px', 
             maxWidth:'900px', 
+            display:'flex',
+            flexDirection: 'column',
         }}>
             <PopupBar />
-            <Box sx={{ width: '100%', height: '100%', backgroundColor: '#EDF5FB', justifyContent:'flex-start'}}>
-                <Container sx={{backgroundColor:'EDF5FB', overflow:'auto', height:'100%', 
+            <Container sx={{backgroundColor:'#EDF5FB', overflow:'auto', height:'100%', 
                     '&::-webkit-scrollbar': {
                         width: '5px'
                     },
@@ -95,7 +97,7 @@ export default function MuiPopup(props: EditPopupProps = data.object) {
               }
                 }}>
                     <Stack spacing={2} sx={{backgroundColor:'EDF5FB'}}>
-                        <Typography variant='h6'  style={{display: 'flex', marginTop: 10 ,justifyContent:'space-between', alignItems:'center'}}>{data.object.name}</Typography>
+                        <Typography variant='h5'  style={{display: 'flex', marginTop: 10 ,justifyContent:'space-between', alignItems:'center'}}>{data.object.name}</Typography>
                             <Stack direction={'row'} spacing={10}  display={'flex'} flex={'flex-start'}>
                                 <Typography>Дата создания {data.object.date_created.toLocaleDateString()}</Typography>
                                 <Typography>Дата изменения {data.object.date_changed.toLocaleDateString()}</Typography>
@@ -143,7 +145,7 @@ export default function MuiPopup(props: EditPopupProps = data.object) {
                             <Grid item md={mdGridSpace+3} xs={12} textAlign='left'>                            
                                     <ButtonGroup size='small' sx={{width: {xs:'100%'}}}>
                                             {statusButtons.map((button) => (
-                                                <Button size='small' sx={{fontSize: {md:'12px',xs:'10px'}}} style={{height: '40px'}} onClick={() =>changeStatus(button.value)} key={statusButtons.indexOf(button)} disabled={status===button.value}>{button.value}</Button>
+                                                <Button size='small'  onClick={() =>changeStatus(button.value)} key={statusButtons.indexOf(button)} disabled={status===button.value}>{button.value}</Button>
                                             ))}
                                     </ButtonGroup>
                             </Grid>
@@ -155,7 +157,7 @@ export default function MuiPopup(props: EditPopupProps = data.object) {
                                 <TextField  size='small' id="outlined-basic" variant="outlined"  defaultValue={data.object.type}/>
                             </Grid>
                             <Grid item md={mdGridSpace+3} xs={4} textAlign='left'>
-                                <Button style={{height: '40px'}} size='small' variant='outlined'>Изменить</Button>
+                                <Button size='small' variant='outlined'>Изменить</Button>
                             </Grid>
 
                             <Grid item md={mdGridName} xs={smGridName}> 
@@ -173,7 +175,7 @@ export default function MuiPopup(props: EditPopupProps = data.object) {
                                 <TextField  size='small' fullWidth id="outlined-basic" variant="outlined"  value={file ? file['name'] : ''} />
                             </Grid>
                             <Grid item md={mdGridSpace} textAlign='left' xs={4}>
-                                <Button style={{height: '40px'}} size='small' variant='outlined' onClick={handleButtonClick}>Изменить</Button>
+                                <Button size='small' variant='outlined' onClick={handleButtonClick}>Изменить</Button>
                                 <input id='input_epw'type="file"  ref={inputRef} onChange={handleFileChange} style={{display: 'none'}} defaultValue={file ? file['name'] : ''}/>
                             </Grid>  
 
@@ -185,7 +187,7 @@ export default function MuiPopup(props: EditPopupProps = data.object) {
 
                             </Grid>
                             <Grid item md={mdGridSpace} textAlign='left' xs={4}>
-                                <Button style={{height: '40px'}} size='small'  variant='outlined'>Изменить</Button>
+                                <Button  size='small'  variant='outlined'>Изменить</Button>
                             </Grid>
                         </Grid>
                     </Stack>
@@ -215,10 +217,9 @@ export default function MuiPopup(props: EditPopupProps = data.object) {
                     </Container>
                     
                 </Container>
-            </Box>
             <MuiDialog formOpen={formOpen} handleCloseForm={handleCloseForm} addLinks={addLinks} />
         </Container>
-      
+    </ThemeProvider>
     </>
   );
 }
