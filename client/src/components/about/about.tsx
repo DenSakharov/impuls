b0dcecbdbf7e095a42f38ea87f82ce5d02240ba9
Sergey Kozhevnikov <SYKozhevnikov@rasu.ru>
2024-04-16 00:48:03 +0300

    Изменил форму About

--- client/src/components/about/about.tsx
+++ client/src/components/about/about.tsx
@@ -1,31 +1,84 @@
 import React from 'react';
-import './about.css';
+import Button from '@mui/material/Button';
+import { styled } from '@mui/material/styles';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+import Typography from '@mui/material/Typography';
 
-function mainPage() {
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+  '& .MuiDialogContent-root': {
+    padding: theme.spacing(2),
+  },
+  '& .MuiDialogActions-root': {
+    padding: theme.spacing(2),
+  },
+}));
+
+function About() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src='./img/logo.png' className="App-logo1" alt="logo" />
-        <h1> О программе!</h1>
-        <h2>Проект IMS "Импульс" (Information Managment System)</h2>
-<p> Система управления информацией “Импульс” — это платформа для совместной работы,
-  обеспечивает взаимодействие между архитектором и стейкхолдерами,
-  которые вносят свой вклад в модель данных проекта и заинтересованы в доступе к информации
-  о важнейших бизнес-процессах, архитектуре программного обеспечения и ИТ-архитектуре предприятия.
-  Потребители используют информацию, полученную из модели данных, для долгосрочного планирования,
-  принятия решений, управления, авторизации и многого другого.
-</p>
-    <a
-          className="App-link"
-          href="http://localhost:3000/main"
-          target=""
-          rel="noopener noreferrer"
-        >
-          Закрыть
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+
+    <Button
+      id="fade-button"
+       aria-controls={open ? 'fade-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClickOpen}>
+      О проекте
+    </Button>
+
+    <BootstrapDialog
+      onClose={handleClose}
+      aria-labelledby="customized-dialog-title"
+      open={open}
+    >
+      <DialogTitle sx={{ m: 0, p: 2 }} id="customized-dialog-title">
+      О проекте
+      </DialogTitle>
+      <IconButton
+        aria-label="close"
+        onClick={handleClose}
+        sx={{
+          position: 'absolute',
+          right: 8,
+          top: 8,
+          color: (theme) => theme.palette.grey[500],
+        }}
+      >
+        <CloseIcon />
+      </IconButton>
+      <DialogContent dividers>
+      <Typography gutterBottom>
+        Система управления информацией “Импульс” — это платформа для совместной работы,обеспечивает взаимодействие между архитектором и стейкхолдерами,
+  которые вносят свой вклад в формирование модели данных проекта и обращаются к информации о ключевых бизнес-процессах, архитектуре программного обеспечения и ИТ-архитектуре предприятия.
+        </Typography>
+        <Typography gutterBottom>
+  Потребители используют информацию, полученную из модели данных, для долгосрочного планирования, принятия решений, управления, авторизации и многого другого.
+        </Typography>
+      </DialogContent>
+
+      <DialogActions>
+        <Button autoFocus onClick={handleClose}>
+          Написать отзыв
+        </Button>
+      </DialogActions>
+      
+    </BootstrapDialog>
+  </React.Fragment>
   );
 }
 
-export default mainPage;
+export default About;
