25deb32830583eda707846939033d90fae01176a
Ilya <ilya.krasnenkov@mail.ru>
2024-04-15 03:09:06 +0300

    добавил тему для мобилы

--- client/src/components/muiPopup/popupBar.tsx
+++ client/src/components/muiPopup/popupBar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Typography, IconButton, Box, Toolbar, AppBar } from '@mui/material';
-import { ArrowDropUp, ArrowDropDown, Close } from '@mui/icons-material';
+import { Typography, IconButton, Box, Toolbar, AppBar, Container, Menu, MenuItem } from '@mui/material';
+import { ArrowDropUp, ArrowDropDown, Close, MoreVert } from '@mui/icons-material';
 import data from '../editPopup/data';
 import MuiChat from './muiChat';
 import MuiHistChngs from './muiHistChngs';
@@ -8,11 +8,17 @@ import { useTheme } from '@emotion/react';
 
 
 export default function PopupBar() {
-  
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const handleHover = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
   return (
-    <Box>
-      <AppBar position="static">
-        <Toolbar sx={{ height: '30px'}}>
+    <Box sx={{flexGrow: 0}}>
+      <AppBar  position="static">
+        <Toolbar sx={{display:'flex', height: '30px'}}>
           <IconButton        
             size="large"
             edge="start"
@@ -34,18 +40,53 @@ export default function PopupBar() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             № {data.object.id}
           </Typography>
-          <MuiChat/>
-          <MuiHistChngs/>
-          <IconButton
-          size="large"
-          edge="start"
-          color="inherit"
-          aria-label="menu"
-          sx={{ mr: 2 }}>
-            <Close fontSize='large'/>
+          <IconButton 
+            
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2, display:{md: 'none', xs: 'block'}}}    
+          onClick={handleHover}>
+            <MoreVert/>
+          </IconButton>
+
+          <Container sx={{display:{md: 'contents', xs: 'none'}}}>
+            <MuiChat />
+            <MuiHistChngs/>
+            <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}>
+              <Close fontSize='large'/>
             </IconButton>
+          </Container>
         </Toolbar>
       </AppBar>
+      <Menu 
+      id='basic-menu'
+      anchorEl={anchorEl}
+      open={Boolean(anchorEl)}
+      onClose={handleClose}
+      sx={{width: 'justifyContent'}}
+      >
+        <MenuItem onClick={handleClose}><MuiChat/></MenuItem>
+        <MenuItem onClick={handleClose}><MuiHistChngs/></MenuItem>
+        <MenuItem onClick={handleClose}><IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}>
+              <Close fontSize='large'/> 
+              <Typography sx={{ml: 2}}>Закрыть</Typography>
+      </IconButton>
+      </MenuItem>   
+    </Menu>
     </Box>
+    
+    
   );
 }
