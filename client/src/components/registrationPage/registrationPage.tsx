488d0550efcc37332a3ddced01b127bd1e725081
RusDa256 <andrey-zharkov@ro.ru>
2024-04-17 19:25:45 +0300

    add page auth and registration

---
+++ client/src/components/registrationPage/registrationPage.tsx
@@ -0,0 +1,99 @@
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+
+export default function Registration() {
+  const handleSubmit = (event: { preventDefault: () => void; currentTarget: HTMLFormElement | undefined; }) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+  };
+
+  return (
+    <Container component="main" maxWidth="sm">
+      <Box
+        sx={{
+          boxShadow: 3,
+          borderRadius: 2,
+          px: 4,
+          py: 6,
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          backgroundColor: 'white',
+        }}
+      >
+        <Typography component="h1" variant="h5">
+          Регистрация
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Придумайте пароль"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Повторите пароль"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Зарегистрироваться
+          </Button>
+
+          <Grid container spacing={3}>
+            <Grid item xs>
+              <Link href="/" underline="none" variant="body2">
+              &lt; &lt; &lt; Авторизация
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="/about" underline="none" variant="body2">
+                {"IMS Impuls"}
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="#" underline="none" variant="body2">
+                {"Контакты"}
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
+  );
+}
