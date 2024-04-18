be9771f38a5fcb624cbb1a026da345cf885fa66a
Ilya <ilya.krasnenkov@mail.ru>
2024-04-18 14:24:33 +0300

    заменил файл манифеста и убрал ворнинги

--- client/src/components/textEditor/FileSaver.tsx
+++ client/src/components/textEditor/FileSaver.tsx
@@ -1,24 +1,18 @@
 import { saveAs } from 'file-saver';
 import * as quillToWord from 'quill-to-word';
-//import * as quill from 'quilljs';
-import Quill from "quill";
-
-
-
 
 async function saver(quillInstance:any) {
     
-   // const quillInstance = new Quill();
-    
-const delta = quillInstance.getContents();
+    // const quillInstance = new Quill();
+        
+    const delta = quillInstance.getContents();
 
-const quillToWordConfig:object = {
-    exportAs: 'blob'
-};
+    const quillToWordConfig:object = {
+        exportAs: 'blob'
+    };
 
-    const docAsBlob:any = await quillToWord.generateWord(delta, quillToWordConfig);
- 
-    saveAs(docAsBlob, 'word-export.docx');
+        const docAsBlob:any = await quillToWord.generateWord(delta, quillToWordConfig);
+        saveAs(docAsBlob, 'word-export.docx');
     
 }
 
