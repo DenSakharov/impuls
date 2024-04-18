1ad63ec8fb8a16e08f4cea31a9b2464522289011
RusDa256 <andrey-zharkov@ro.ru>
2024-04-18 16:05:17 +0300

    fix

--- client/src/components/registrationPage/registrationPage.tsx
+++ client/src/components/registrationPage/registrationPage.tsx
@@ -1,11 +1,6 @@
 import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
-import Link from "@mui/material/Link";
 import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import { Container } from "@mui/material";
 import logo from "./logo_reg.png"
@@ -51,7 +46,7 @@ export default function Registration() {
             alignItems: "center",
             backgroundColor: 'white',
           }}>
-          <img src={`${logo}`} width={300}/>
+          <img src={`${logo}`} width={300} alt="logo"/>
           <Box
               sx={{
                 my: 4,
