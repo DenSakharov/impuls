2dbc21a9ec69d5794ee2591beb8c729706408b34
Mikhail Ikonnikov <mishaikon@gmail.com>
2024-04-19 14:56:35 +0300

    IJF-35 fix to separate project styles from other styles (WORKS)

--- client/src/components/projects/header.tsx
+++ client/src/components/projects/header.tsx
@@ -6,7 +6,7 @@ function Header() {
             <div className="mdl-layout__header-row">
                 <span className="mdl-layout-title">СУИ “Импульс”</span>
                 <div className="mdl-layout-spacer"></div>
-                <span>Пользователь: Иванов П.В. ( <a href="abc.com">Выйти</a> )</span>
+                <span>Пользователь: Иванов П.В. ( <a href="#">Выйти</a> )</span>
             </div>
         </header>
     )
