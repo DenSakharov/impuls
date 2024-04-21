f8c3dc519d7390ffe2da89ccd175ee0b7c2ceb29
SergeyKozhevnikov <kozhevnikov.su@gmail.com>
2024-04-22 00:38:54 +0300

    добавил первые компоненты tailWind

--- client/src/components/mainPage/mainPage.tsx
+++ client/src/components/mainPage/mainPage.tsx
@@ -35,7 +35,7 @@ export default function MainPage() {
   const handleOpenForm = () => {
   setFormOpen(true);
     }
-    
+
 
   return (
 <div className="mainApp mainApp-push-bottom">
@@ -66,7 +66,7 @@ export default function MainPage() {
        <Container fixed>
         <Box sx={{ bgcolor: '#F5F5F5', height: '10vh' }} >
         <div className="container-news">
-        <h2> Новости </h2>
+        <h1 className="text-3xl font-bold underline"> Новости </h1>
          <Box sx={{ width: '100%' }}>
         <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
         <Grid xs={6}>
@@ -93,8 +93,8 @@ export default function MainPage() {
 
        <Container fixed>
         <Box sx={{ bgcolor: '#FFFFFF', height: '70vh' }} >
-        <h2> Ключевые показатели  </h2> 
-          <div className="container-kpi1">           
+        <h1 className="text-3xl font-bold underline"> Ключевые показатели  </h1>
+          <div className="container-kpi1">
            </div>
         </Box>
       </Container>
@@ -102,7 +102,7 @@ export default function MainPage() {
       <Container fixed>
         <Box sx={{ bgcolor: '#F5F5F5', height: '10vh'}} >
         <div className="container-proj">
-        <h2> Информация проекта</h2> 
+        <h1 className="text-3xl font-bold underline"> Информация проекта</h1>
    	  </div>
         </Box>
       </Container>
@@ -120,6 +120,27 @@ export default function MainPage() {
     </div>
 
 </footer>
+
+
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+
+    <div className="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+  <img className="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" src="/img/erin-lindford.jpg" alt="Woman's Face" />
+  <div className="text-center space-y-2 sm:text-left">
+    <div className="space-y-0.5">
+      <p className="text-lg text-black font-semibold">
+        Erin Lindford
+      </p>
+      <p className="text-slate-500 font-medium">
+        Product Engineer
+      </p>
+    </div>
+    <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Message</button>
+  </div>
+</div>
+
 </div>
 );
 }
