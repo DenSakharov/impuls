c44f89fb37de88be5470f9a676b3a2da7abc199e
SergeyKozhevnikov <kozhevnikov.su@gmail.com>
2024-04-23 01:59:40 +0300

    Доработка страницы с новыми компонентами

--- client/src/components/mainPage/testpage.tsx
+++ client/src/components/mainPage/testpage.tsx
@@ -3,42 +3,52 @@ import  React  from 'react';
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
-
-import MuiMenu from './muiMenu';
-import MuiTree from './muiTree';
-import MuiButTree from './muiButTree';
 import About from './../about/about';
 import Contacts from '../about/contacts_teem';
-
 import data from '../editPopup/data';
-import { Dialog } from '@mui/material';
-import MuiPopup from '../muiPopup/muiPopup';
-import { Container } from '@mui/system';
-import Grid from '@mui/system/Unstable_Grid';
 import styled from '@mui/system/styled';
-import Box from '@mui/system/Box';
 
 
+const Item = styled('div')(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  border: '1px solid #F5F5F5' ,
+  borderColor: theme.palette.mode === 'dark' ? '#444d58' : '#ced7e0',
+  padding: theme.spacing(1),
+  borderRadius: '4px',
+  textAlign: 'left',
+}));
+
+
+const service = {
+  name: 'serv',
+  email: 'ii@rosatom.ru',
+  imageUrl: '../../img/settings2_32px.png',
+}
 
 const user = {
-  name: 'Tom Cook',
-  email: 'tom@example.com',
+  name: 'Иванов Иван',
+  email: 'ii@rosatom.ru',
   imageUrl:
-    'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    '../../img/male_user_50px.png',
 }
 
 const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
-  { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
-  { name: 'Reports', href: '#', current: false },
+  { name: 'Дашборд', href: '#', current: true },
+  { name: 'Поиск', href: '#', current: false },
+  { name: 'Отчеты', href: '#', current: false },
+]
+const serviceNavigation = [
+  { name: 'Проекты и задачи', href: '#' },
+  { name: 'История согласований', href: '#' },
+  { name: 'Импорт/Экспорт', href: '#' },
+  { name: 'Текстовый редактор', href: '#' },
 ]
 const userNavigation = [
-  { name: 'Your Profile', href: '#' },
-  { name: 'Settings', href: '#' },
-  { name: 'Sign out', href: '#' },
+  { name: 'Профиль', href: '#' },
+  { name: 'О проекте', href: '#' },
+  { name: 'Выйти', href: '#' },
 ]
+
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
@@ -57,12 +67,10 @@ function Testpage() {
     setFormOpen(true);
       }
 
-
-
     return (
           <>
       <div className="min-h-full">
-        <Disclosure as="nav" className="bg-gray-800">
+        <Disclosure as="nav" className="bg-sky-800">
           {({ open }) => (
             <>
               <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
@@ -71,8 +79,8 @@ function Testpage() {
                     <div className="flex-shrink-0">
                       <img
                         className="h-8 w-8"
-                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                        alt="Your Company"
+                        src="./img/logo.png"
+                        alt="IMS Impuls"
                       />
                     </div>
                     <div className="hidden md:block">
@@ -92,9 +100,48 @@ function Testpage() {
                             {item.name}
                           </a>
                         ))}
+
+                        {/* Service dropdown */}
+                        <Menu as="div" className="relative ml-3">
+                        <div className="rounded-md flex items-baseline space-x-4 text-gray-300 hover:bg-gray-700 hover:text-white">
+                          <Menu.Button className="relative rounded-md px-3 py-2 text-sm font-medium">
+                           <a className=""> Сервисы </a>
+                          </Menu.Button>
+                        </div>
+
+                        <Transition
+                          as={Fragment}
+                          enter="transition ease-out duration-100"
+                          enterFrom="transform opacity-0 scale-95"
+                          enterTo="transform opacity-100 scale-100"
+                          leave="transition ease-in duration-75"
+                          leaveFrom="transform opacity-100 scale-100"
+                          leaveTo="transform opacity-0 scale-95"
+                        >
+                          <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                            {serviceNavigation.map((item) => (
+                              <Menu.Item key={item.name}>
+                                {({ active }) => (
+                                  <a
+                                    href={item.href}
+                                    className={classNames(
+                                      active ? 'bg-gray-100' : '',
+                                      'block px-4 py-2 text-sm text-gray-700'
+                                    )}
+                                  >
+                                    {item.name}
+                                  </a>
+                                )}
+                              </Menu.Item>
+                            ))}
+                          </Menu.Items>
+                        </Transition>
+                      </Menu>
                       </div>
                     </div>
                   </div>
+
+
                   <div className="hidden md:block">
                     <div className="ml-4 flex items-center md:ml-6">
                       <button
@@ -106,7 +153,10 @@ function Testpage() {
                         <BellIcon className="h-6 w-6" aria-hidden="true" />
                       </button>
 
-                      {/* Profile dropdown */}
+
+
+
+{/* Profile dropdown */}
                       <Menu as="div" className="relative ml-3">
                         <div>
                           <Menu.Button className="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
@@ -115,6 +165,7 @@ function Testpage() {
                             <img className="h-8 w-8 rounded-full" src={user.imageUrl} alt="" />
                           </Menu.Button>
                         </div>
+
                         <Transition
                           as={Fragment}
                           enter="transition ease-out duration-100"
@@ -146,8 +197,10 @@ function Testpage() {
                     </div>
                   </div>
                   <div className="-mr-2 flex md:hidden">
-                    {/* Mobile menu button */}
-                    <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+{/* END Profile dropdown */}
+
+{/* Mobile menu button */}
+          <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                       <span className="absolute -inset-0.5" />
                       <span className="sr-only">Open main menu</span>
                       {open ? (
@@ -155,11 +208,10 @@ function Testpage() {
                       ) : (
                         <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
                       )}
-                    </Disclosure.Button>
+          </Disclosure.Button>
                   </div>
                 </div>
               </div>
-
               <Disclosure.Panel className="md:hidden">
                 <div className="space-y-1 px-2 pb-3 pt-2 sm:px-3">
                   {navigation.map((item) => (
@@ -176,7 +228,23 @@ function Testpage() {
                       {item.name}
                     </Disclosure.Button>
                   ))}
+                  
+                  <div className="mt-3 space-y-1 px-2">
+                    {serviceNavigation.map((item) => (
+                      <Disclosure.Button
+                        key={item.name}
+                        as="a"
+                        href={item.href}
+                        className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
+                      >
+                        {item.name}
+                      </Disclosure.Button>
+
+                    ))}
+                  </div>
                 </div>
+
+
                 <div className="border-t border-gray-700 pb-3 pt-4">
                   <div className="flex items-center px-5">
                     <div className="flex-shrink-0">
@@ -186,6 +254,7 @@ function Testpage() {
                       <div className="text-base font-medium leading-none text-white">{user.name}</div>
                       <div className="text-sm font-medium leading-none text-gray-400">{user.email}</div>
                     </div>
+
                     <button
                       type="button"
                       className="relative ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
@@ -195,6 +264,7 @@ function Testpage() {
                       <BellIcon className="h-6 w-6" aria-hidden="true" />
                     </button>
                   </div>
+
                   <div className="mt-3 space-y-1 px-2">
                     {userNavigation.map((item) => (
                       <Disclosure.Button
@@ -212,43 +282,27 @@ function Testpage() {
             </>
           )}
         </Disclosure>
+        {/* END Mobile menu button */}
 
         <header className="bg-white shadow">
           <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <h1 className="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
+            <h1 className="text-3xl font-bold tracking-tight text-gray-900">Дашборд</h1>
           </div>
         </header>
+
         <main>
           <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-            {/* Your content */}
+          {/* Your content */}
 
-            <div className="aside">
-            <MuiButTree/>
-   	        <MuiTree data={projectData} handleOpenForm={handleOpenForm} setPopupData={setPopupData}/>
-  	        </div>
+          </div>
+        </main>
 
-            <div className="content">
-              <div id="containerHeader">
-              <div id="mainHeader1">
-        			  <div className="content-text-block">
-                </div>
-              </div>
-              </div>
-            </div>
-
-
-           <footer id="content">
+        <footer id="content">
            <div id='foot'>
               <p> <About/> | <Contacts/> </p>
               <p id="copyright"><a href="http://localhost:3000" target="_blank" rel="noreferrer">© Impuls Teem 2024</a></p>
           </div>
           </footer>
-
-
-
-
-          </div>
-        </main>
       </div>
     </>
 
