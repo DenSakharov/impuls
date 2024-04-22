97582d9d63038853a6d4a819eb0df38590604abb
Mikhail Ikonnikov <mishaikon@gmail.com>
2024-04-22 23:39:47 +0300

    IJF-53 create addDirectory modal popup

--- client/src/components/mainPage/muiButTree.tsx
+++ client/src/components/mainPage/muiButTree.tsx
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IconButton, Stack } from '@mui/material';
 import { Settings, Info, ContentCopy, Add } from '@mui/icons-material';
 import DeleteIcon from '@mui/icons-material/Delete';
-
+import MuiAddDirectory from "../muiPopup/muiAddDirectory";
 
 function MuiButTree() {
     // const classes = useStyles()
 
+    const [isModalOpen, setModalOpen] = useState(false);
+
+    const openModal = () => setModalOpen(true);
+    const closeModal = () => setModalOpen(false);
+
     return (
         <Stack spacing={1} direction="row">
-        <IconButton aria-label="settings" color='default' size="small"><Add fontSize="small"/></IconButton>
-        <IconButton aria-label="settings" color='default' size="small"><ContentCopy fontSize="small" /></IconButton>
-        <IconButton edge='end' aria-label="delete" color='default' size="small"> <DeleteIcon fontSize="small"/></IconButton>
-        <IconButton aria-label="info" color='default' size="small"><Info fontSize="small"/></IconButton>
-        <IconButton aria-label="settings" color='default' size="small"><Settings fontSize="small"/></IconButton>
+            <IconButton aria-label="settings" color='default' size="small" onClick={openModal}><Add fontSize="small"/></IconButton>
+            <IconButton aria-label="settings" color='default' size="small"><ContentCopy fontSize="small" /></IconButton>
+            <IconButton edge='end' aria-label="delete" color='default' size="small"> <DeleteIcon fontSize="small"/></IconButton>
+            <IconButton aria-label="info" color='default' size="small"><Info fontSize="small"/></IconButton>
+            <IconButton aria-label="settings" color='default' size="small"><Settings fontSize="small"/></IconButton>
+            <MuiAddDirectory isOpen={isModalOpen} onClose={closeModal} />
         </Stack>
     );
   }
