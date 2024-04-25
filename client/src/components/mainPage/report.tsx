428e82276a165756da6b0e793e025252880a4463
SergeyKozhevnikov <kozhevnikov.su@gmail.com>
2024-04-26 02:05:33 +0300

    Добавил страницу Report

---
+++ client/src/components/mainPage/report.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Report() {
+  return (
+
+<>
+<span>Report</span>
+
+           <h2 className="text-3xl font-bold tracking-tight text-gray-900">Report</h2>
+
+
+</>
+  );
+}
+
+export default Report;
