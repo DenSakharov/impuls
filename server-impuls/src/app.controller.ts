9fd0c44fd934df88cec151048354f419f7536ea1
Ilya <ilya.krasnenkov@mail.ru>
2024-04-26 16:26:42 +0300

    empty

--- server-impuls/src/app.controller.ts
+++ server-impuls/src/app.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('/app')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('helloWorld')
   getHello(): string {
     return this.appService.getHello();
   }
