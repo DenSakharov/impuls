9823ecd9c4c82600a227d7257a8cd4aa177f9684
Ilya <ilya.krasnenkov@mail.ru>
2024-04-26 19:25:16 +0300

    type-orm -> sequalize

--- server-impuls/src/app.module.ts
+++ server-impuls/src/app.module.ts
@@ -1,32 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DatabaseModule } from './database/database.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { databaseProviders } from './database.providers';
+
 
 @Module({
-  imports: [
-    DatabaseModule, 
-    ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: configService.get('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_NAME'),
-        synchronize: true,
-        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-      }),
-      inject: [ConfigService],
-    })
-  ],
+  imports: [],
   controllers: [AppController],
-  providers: [AppService],
-  exports: [],
+  providers: [
+    AppService,
+    ...databaseProviders
+  ],
+  exports: [
+    ...databaseProviders
+  ],
 })
 export class AppModule {}
 console.log(process.env)
