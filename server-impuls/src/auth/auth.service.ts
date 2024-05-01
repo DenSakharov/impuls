b12d5a4a040829f0fb706ea455e6e5a92f914cbb
Ilya <ilya.krasnenkov@mail.ru>
2024-05-01 16:57:16 +0300

    Получение JWT, авторизация через /login,криптография

---
+++ server-impuls/src/auth/auth.service.ts
@@ -0,0 +1,33 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { tSecuserService } from '#/tSecuser/tSecuser.service';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+
+export interface JwtPayload {
+  userlogin: string;
+}
+
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private usersService: tSecuserService,
+    private jwtService: JwtService,
+  ) {}
+
+  async signIn(
+    userlogin: string,
+    pass: string,
+  ): Promise<{ accessToken: string }> {
+
+    const user = await this.usersService.findOne(userlogin);
+
+    if (!(user && (await bcrypt.compare(pass, user.password)))) {
+      throw new UnauthorizedException('Please check your login credentials');
+    }
+
+    const payload: JwtPayload = { userlogin };
+    const accessToken: string = await this.jwtService.sign(payload);
+    return { accessToken };
+  }
+}
