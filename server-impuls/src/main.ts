4553b37b68e9e3e557293e2c77975bd043356109
Ilya <ilya.krasnenkov@mail.ru>
2024-05-19 12:08:58 +0300

    Добавил контейнеры для продакшн билдов

--- server-impuls/src/main.ts
+++ server-impuls/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000'
+    origin: true
   })
   await app.listen(3010);
 }
