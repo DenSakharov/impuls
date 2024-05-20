4d8687cc0d262f4be23876b45cba9a77ff1b94b0
RusDa256 <andrey-zharkov@ro.ru>
2024-05-20 10:37:47 +0300

    доработка страницы профиля

--- client/src/components/profile/profile.tsx
+++ client/src/components/profile/profile.tsx
@@ -1,8 +1,59 @@
 import './style_profile.css';
 import bBack from './img/bBack.svg'
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios, { AxiosError, AxiosResponse } from "axios";
+import Snackbar from '@mui/material/Snackbar';
+import Alert from '@mui/material/Alert';
 
 function Profile() {
+
+  var [userLogin, setUserLogin] = useState('');
+  var [userSurname, setUserSurname] = useState('');
+  var [userFirstname, setUserFirstname] = useState('');
+  var [userEmail, setUserEmail] = useState('');
+  var [userDepartment, setUserDepartment] = useState('');
+
+  const [openSnackbar, setOpenSnackbar] = React.useState(false);
+
+  const handleClick = () => {
+    setOpenSnackbar(true);
+  };
+
+  const handleClose = (event: React.SyntheticEvent | Event, reason?: string) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpenSnackbar(false);
+  };
+
+  function getUser() {
+    axios({
+      method: 'get',
+      url: 'http://localhost:3010/users/User'
+    }).then((response: AxiosResponse) => {
+      console.log(response.data)
+
+      var User = response.data
+      setUserLogin(User.userlogin)
+      setUserSurname(User.surname)
+      setUserFirstname(User.firstname)
+      setUserEmail(User.userEmail)
+      setUserDepartment(User.department)
+
+      handleClick()
+    }).catch((reason: AxiosError) => {
+      console.log(reason)
+    })
+  }
+
+  useEffect(() => {
+    let userReceived = false;
+    
+    if (!userReceived)  getUser()
+    return () => { userReceived = true; }
+    },[]);
+
   return(
     <div id="page-profile">
       <header id="header-profile">
@@ -34,15 +85,15 @@ function Profile() {
           <div id="infoUser">
             <div className="infoUserValues" id="el1">
               <label>Логин</label>
-              <input type="text"/>
+              <input defaultValue={userLogin} type="text"/>
             </div>
             <div className="infoUserValues" id="el2">
               <label>Email</label>
-              <input type="text"/>
+              <input defaultValue={userEmail} type="text"/>
             </div>
             <div className="infoUserValues" id="el3">
               <label>Фамилия</label>
-              <input type="text"/>
+              <input defaultValue={userSurname} type="text"/>
             </div>
             <div className="infoUserValues" id="el4">
               <label>Телефон</label>
@@ -50,11 +101,11 @@ function Profile() {
             </div>
             <div className="infoUserValues" id="el5">
               <label>Имя</label>
-              <input type="text"/>
+              <input defaultValue={userFirstname} type="text"/>
             </div>
             <div className="infoUserValues" id="el6">
               <label>Должность</label>
-              <input type="text"/>
+              <input defaultValue={userDepartment} type="text"/>
             </div>
             <div className="infoUserValues" id="el7">
               <label>Отчество</label>
@@ -81,6 +132,19 @@ function Profile() {
           </div>
         </div>
       </div>
+      <Snackbar
+        open={openSnackbar}
+        autoHideDuration={5000}
+        onClose={handleClose}>
+          <Alert
+            onClose={handleClose}
+            severity="success"
+            variant="filled"
+            sx={{ width: '100%' }}
+          >
+            Изменения успешно сохранены!
+          </Alert>
+        </Snackbar>\
     </div>
   )
 }
