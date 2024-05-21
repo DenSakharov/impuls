2058d1279a42c31e9d8167013261d4cde69c292d
Ilya <ilya.krasnenkov@mail.ru>
2024-05-21 18:41:11 +0300

    изменение tSecuser.controller и tSecuser.service

--- server-impuls/src/tSecuser/tSecuser.controller.ts
+++ server-impuls/src/tSecuser/tSecuser.controller.ts
@@ -2,20 +2,19 @@ import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { tSecuserService } from '#/tSecuser/tSecuser.service';
 import { tSecuser } from '#/tSecuser/tSecuser';
 import { AuthGuard } from '#/auth/auth.guard';
-import { IMessage } from '#/entities/Message';
 
 @Controller('/users')
 export class TSecuserController {
   constructor(private readonly tSecuserService: tSecuserService) {}
-
+  
   @UseGuards(AuthGuard)
-  @Get('/')
+  @Get('/all')
   findAll(): Promise<tSecuser[]> {
     return this.tSecuserService.findAll();
   }
 
-  @Post('/')
-  create(@Body() newUser: tSecuser): Promise<IMessage> {
+  @Post('/create')
+  create(@Body() newUser: tSecuser): Promise<string> {
     return this.tSecuserService.create(newUser);
   }
 
--- server-impuls/src/tSecuser/tSecuser.service.ts
+++ server-impuls/src/tSecuser/tSecuser.service.ts
@@ -1,7 +1,6 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { tSecuser } from '#/tSecuser/tSecuser';
 import * as bcrypt from 'bcrypt';
-import { IMessage } from '#/entities/Message';
 
 @Injectable()
 export class tSecuserService {
@@ -10,10 +9,10 @@ export class tSecuserService {
     private tSecuserRepository: typeof tSecuser,
   ) {}
 
-  async create(newUser: Partial<tSecuser>): Promise<IMessage> {
-    const hashedPassword = await bcrypt.hash(newUser.password, 10);
-    const userUUID = crypto.randomUUID();
-    try {
+  async create(newUser: Partial<tSecuser>): Promise<string> {
+      const hashedPassword = await bcrypt.hash(newUser.password, 10);
+      const userUUID = crypto.randomUUID();
+      try {
       await this.tSecuserRepository.create({
         userid: userUUID,
         userlogin: newUser.userlogin,
@@ -24,13 +23,16 @@ export class tSecuserService {
         password: hashedPassword,
         groupid: newUser.groupid,
       });
-      return { message: `created new user with id = ${userUUID} ` };
-    } catch (error) {
+      return `created new user with id = ${userUUID} `;
+
+    } catch(error) {
+
       if (error.name === 'SequelizeUniqueConstraintError') {
-        return { error: 'This login already exists' };
+        return 'This login already exists';
       } else {
-        return { error: error.name };
+        return error.name;        
       }
+
     }
   }
 
