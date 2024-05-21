db7bd7e0cadfef417dd531c05d29bf33d85d7161
SegeyKozhevnikov <kozhevnikov.su@gmail.com>
2024-05-21 18:04:48 +0300

    Доработки startpage

--- client/src/components/mainPage/main.tsx
+++ client/src/components/mainPage/main.tsx
@@ -60,7 +60,6 @@ function Main({changeState} : any) {
       }  
       setOpen(false);
     };
-
     
     return (
       <>
--- client/src/components/mainPage/muiStartpage.tsx
+++ client/src/components/mainPage/muiStartpage.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
 import { Card, Typography, Box } from '@mui/material';
+import SelectProjectsNew from './selectProjectsNew';
+import data from '../editPopup/data';
 
 const MuiStartpage: React.FC = () => {
+
+    // const [popupData, setPopupData] = React.useState(data.object);
+    const [projectData, setProjectData] = React.useState(data.tree[0]);  
+    // const [formOpen, setFormOpen] = React.useState(false);  
+
+    // const handleCloseForm = () => {
+    // setFormOpen(false);
+    // }
+    // const handleOpenForm = () => {
+    // setFormOpen(true);
+    // }
+
     return (
         <Card sx={{ padding: '20px', margin: '20px' }}>
             <Typography variant="h5" gutterBottom>Стартовая страница</Typography>
@@ -9,7 +23,10 @@ const MuiStartpage: React.FC = () => {
              <div className="flex flex-wrap items-center gap-8 ">
                <div className="max-w-7xl py-1 sm:px-6 lg:px-6">
                   <Typography variant="h6" color="colorPrimary" gutterBottom> Открыть </Typography>
-                  <Typography color="textSecondary" gutterBottom> Открыть проект </Typography>
+                  <Typography color="textSecondary" gutterBottom>                                       
+                    <SelectProjectsNew changeState={setProjectData}/>             
+                  </Typography>
+                   
                   <Typography color="textSecondary" gutterBottom> Настроить </Typography>
                </div>
                <div className="max-w-7xl py-1 sm:px-6 lg:px-8">
