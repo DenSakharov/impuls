b013b80e0106b76c52d27123f2f00120b90fa4dc
Ilya <ilya.krasnenkov@mail.ru>
2024-05-24 15:38:45 +0300

    Revert "Merge branch 'main' into muiPopupApi"
    This reverts commit 17bb26f617e79cbf0f874f35e4b5d97ea5c9b320, reversing
    changes made to 73c830e980b0f42af37bf7774f801a4a96c443f5.

--- client/src/components/mainPage/muiStartpage.tsx
+++ client/src/components/mainPage/muiStartpage.tsx
@@ -18,8 +18,8 @@ const MuiStartpage: React.FC = () => {
     // }
 
     return (
-        <Card sx={{ padding: '10px', margin: '10px' }}>
-            <Typography variant="h5" gutterBottom sx={{ flexGrow: 1, textAlign: "center"}}>Стартовая страница</Typography>
+        <Card sx={{ padding: '20px', margin: '20px' }}>
+            <Typography variant="h5" gutterBottom>Стартовая страница</Typography>
             <Box>
              <div className="flex flex-wrap items-center gap-8 ">
                <div className="max-w-7xl py-1 sm:px-6 lg:px-6">
--- server-impuls/src/news/dto/update-news.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateNewsDto } from './create-news.dto';
-
-export class UpdateNewsDto extends PartialType(CreateNewsDto) {}
