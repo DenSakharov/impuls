9e8a70d550f737b7b7b33bb92209808435d1eba8
VladimirPikhtovnikov <pvvs09@gmail.com>
2024-05-29 00:48:29 +0300

    Update data.tsx

--- client/src/components/CordinationPage/data.tsx
+++ client/src/components/CordinationPage/data.tsx
@@ -1,36 +1,10 @@
 export interface RecordI {
     id:number;
-    name:string;
-    DataCreate:string;
-    Autohr:string;
-    DataCordination:string;
     Cordinator:string;
-    Status:string;
-}
+    DataCordination:string;
+    Comment:string;
+    Status: boolean;
+    Approved: boolean;   
+    
+} 
 
-export const records: RecordI[] = [
-    {
-        id:1, name:"Проект Импульс",
-        DataCreate:"01/04/2024",
-        Autohr: "Пихтовников В.В.",
-        DataCordination:"01/04/2024",
-        Cordinator:"Федоров",
-        Status:"Согласован"
-    },
-    {
-        id:2, name:"Проект Импульс",
-        DataCreate:"01/04/2024",
-        Autohr: "Пихтовников В.В.",
-        DataCordination:"01/04/2024",
-        Cordinator:"Федоров",
-        Status:"Согласован"
-    },
-    {
-        id:3, name:"Проект Импульс",
-        DataCreate:"01/04/2024",
-        Autohr: "Пихтовников В.В.",
-        DataCordination:"01/04/2024",
-        Cordinator:"Федоров",
-        Status:"Согласован"
-    },        
-]
