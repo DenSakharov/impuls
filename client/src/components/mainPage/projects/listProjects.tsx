64851698ec4e118f87413b3de93c485cfdd15a34
Dezmond-sama <Dezmond-sama@mail.ru>
2024-05-30 20:39:58 +0300

    Добавлено удаление проектов.

--- client/src/components/mainPage/projects/listProjects.tsx
+++ client/src/components/mainPage/projects/listProjects.tsx
@@ -12,6 +12,7 @@ import { Button, TextField, Dialog, DialogActions, DialogContent, DialogTitle } 
 import Snackbar from '@mui/material/Snackbar';
 import EditProjectsModal from './editProjects';
 import { tProjectAttributes } from '#/dtos';
+import MuiDeleteElement from '../muiDeleteElement';
 // import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
 // import { useDemoData } from '@mui/x-data-grid-generator';
 
@@ -27,9 +28,13 @@ export type ListProjectsProps = {
 }
 const ListProjects = ({ projects, reload }: ListProjectsProps) => {
   const [showModalEdit, setShowModalEdit] = useState(false);
+  const [showModalDelete, setShowModalDelete] = useState(false);
+  
+  const openModalDelete = () => setShowModalDelete(true);
+  const closeModalDelete = () => setShowModalDelete(false);
 
   //const [tableData, setTableData] = useState([])
-  const [selectedItem, setSelectedItem] = useState(null);
+  const [selectedItem, setSelectedItem] = useState<tProjectAttributes|null>(null);
   //const [rows, setRows] = useState(tableData);
   //const [deletedRows, setDeletedRows] = useState([]);
 
@@ -40,47 +45,62 @@ const ListProjects = ({ projects, reload }: ListProjectsProps) => {
   };
 
   //const [selectedRows, setSelectedRows] = React.useState([]);
-  const editClickHandler = (item) => {
+  const editClickHandler = (item: tProjectAttributes) => {
     setSelectedItem(item);
     setShowModalEdit(true);    
   }
+  const deleteClickHandler = (item: tProjectAttributes) => {
+    setSelectedItem(item);
+    openModalDelete();
+  }
+
   return (
-    <TableContainer component={Paper} >
-      <Table
-        aria-labelledby="tableTitle"
-        size='small'>
-        <TableHead>
-          <TableRow>
-            <TableCell> UUID </TableCell>
-            <TableCell> Название проекта </TableCell>
-            <TableCell align="center"> Статус </TableCell>
-            {/* <TableCell align="left"> Описание </TableCell>     */}
-            <TableCell align="center"> Действия </TableCell>
-          </TableRow>
-        </TableHead>
-
-        <TableBody>
-          {projects && projects.map((item, index) => (
-            <TableRow
-              key={index}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              // onSelectionChange={(newSelection) => {setSelection(newSelection.rows) }}
-            >
-              <TableCell component="th" scope="row"> {item.projectId}</TableCell>
-              <TableCell component="th" scope="row"> {item.name}</TableCell>
-              <TableCell align="center">{item.status}</TableCell>
-              {/* <TableCell align="left">{item.notes}</TableCell> */}
-              <TableCell align="center">
-                <div><Button variant="text" onClick={()=>editClickHandler(item)}> Изменить </Button>
-
-                <Button variant="text" onClick={() => {}}> Удалить  </Button></div>
-              </TableCell>
+    <>
+      <TableContainer component={Paper} >
+        <Table
+          aria-labelledby="tableTitle"
+          size='small'>
+          <TableHead>
+            <TableRow>
+              <TableCell> UUID </TableCell>
+              <TableCell> Название проекта </TableCell>
+              <TableCell align="center"> Статус </TableCell>
+              {/* <TableCell align="left"> Описание </TableCell>     */}
+              <TableCell align="center"> Действия </TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-        {showModalEdit && selectedItem && <EditProjectsModal projectsItem={selectedItem} onClose={handleCloseModalEdit} onSuccessCallback={reload}/>}
-    </TableContainer>
+          </TableHead>
+
+          <TableBody>
+            {projects && projects.map((item, index) => (
+              <TableRow
+                key={index}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                // onSelectionChange={(newSelection) => {setSelection(newSelection.rows) }}
+              >
+                <TableCell component="th" scope="row"> {item.projectId}</TableCell>
+                <TableCell component="th" scope="row"> {item.name}</TableCell>
+                <TableCell align="center">{item.status}</TableCell>
+                {/* <TableCell align="left">{item.notes}</TableCell> */}
+                <TableCell align="center">
+                  <div><Button variant="text" onClick={() => editClickHandler(item)}> Изменить </Button>
+
+                  <Button variant="text" onClick={() => deleteClickHandler(item)}> Удалить  </Button></div>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+          {showModalEdit && selectedItem && <EditProjectsModal projectsItem={selectedItem} onClose={handleCloseModalEdit} onSuccessCallback={reload}/>}
+      </TableContainer>
+      {selectedItem && <MuiDeleteElement
+          title="Удалить проект" 
+          element={selectedItem}
+          projectId={""}
+          onSuccessCallback={reload}
+          isOpen={showModalDelete}
+          onClose={closeModalDelete}
+      />}
+    </>
 );
 }
 
